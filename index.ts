e3265f83b7da24d1761732c08bc1e8892cccb5d5
Gragy08 <giaqui1712@gmail.com>
2025-06-18 11:47:52 +0700

    Tính năng Đăng ký (Ứng viên) - BE

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import cors from "cors";
+import routes from "./routes/index.route";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+
+// Nạp biến môi trường từ file .env
+dotenv.config();
 
 const app = express();
 const port = 4000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from Project 2 Backend!');
-});
+// Kết nối đến cơ sở dữ liệu
+connectDB();
+
+// Cấu hình CORS
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Cho phép gửi data lên dạng json
+app.use(express.json());
+
+// Thiết lập đường dẫn
+app.use("/", routes);
 
 app.listen(port, () => {
     console.log(`Project 2 Backend is running at http://localhost:${port}`);
